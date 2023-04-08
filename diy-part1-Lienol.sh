ea307ea1281fabbf6646777b0adf9a2bfdb05836
microstone <microstone@gmail.com>
2023-04-08 17:08:41 +0800

    Update diy-part1-Lienol.sh

--- diy-part1-Lienol.sh
+++ diy-part1-Lienol.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git other https://github.com/Lienol/openwrt-package.git;other' >>feeds.conf.default
+# echo 'src-git other https://github.com/Lienol/openwrt-package.git;other' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
